5c663a66d7df427a78b102becdc3d1ce18ce2bbf
Kim Pihlström <kim.pihlstrom@gmail.com>
2015-11-03 13:42:46 +0200

    Initial check-in. Based on source code from http://managedwifi.codeplex.com/.

---
+++ Windows.WifiNetworkInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Common.Wifi;
+
+namespace Windows.WifiNetworkInfo
+{
+    class Program
+    {
+        /// <summary>
+        /// Converts a 802.11 SSID to a string.
+        /// </summary>
+        static string GetStringForSSID(Wlan.Dot11Ssid ssid)
+        {
+            return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+        }
+
+        static void Main(string[] args)
+        {
+            WlanClient client = new WlanClient();
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+                // Lists all networks with WEP security
+                Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+                foreach (Wlan.WlanAvailableNetwork network in networks)
+                {
+                    Console.WriteLine("Found WEP network with SSID {0} and signal quality {1}.", GetStringForSSID(network.dot11Ssid), network.wlanSignalQuality);
+                }
+            }
+        }
+    }
+}
